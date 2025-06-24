75d26370eb4a74f4192ad7d6fef947e3d08316b9
Yaikob Wasihun <wyaikob@gmail.com>
2025-06-24 09:52:11 +0300

    user activation using email

--- Asbezacart/settings.py
+++ Asbezacart/settings.py
@@ -145,3 +145,10 @@ MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'imewase@gmail.com'
+EMAIL_HOST_PASSWORD = 'wlndcrcjbmvxlihj'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'imewase@gmail.com'
